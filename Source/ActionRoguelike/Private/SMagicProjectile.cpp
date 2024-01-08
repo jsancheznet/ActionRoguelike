b9e1388e018fb812102fb2bc56d4fbe80b6d6025
jsanchez.net <jsanchezsilvera@gmail.com>
2024-01-08 17:36:16 -0300

    Fixed collision settings with world and magic projectile

--- Source/ActionRoguelike/Private/SMagicProjectile.cpp
+++ Source/ActionRoguelike/Private/SMagicProjectile.cpp
@@ -11,6 +11,7 @@
 ASMagicProjectile::ASMagicProjectile()
 {
 	SphereComp->SetSphereRadius(20.0f);
+	SphereComp->OnComponentBeginOverlap.AddDynamic(this, &ASMagicProjectile::OnActorOverlap);	
 
 	AudioComp = CreateDefaultSubobject<UAudioComponent>("AudioComp");
 	AudioComp->SetupAttachment(SphereComp);
@@ -18,7 +19,7 @@ ASMagicProjectile::ASMagicProjectile()
 	DamageAmount = 20.0f;
 }
 
-void ASMagicProjectile::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+void ASMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	if(OtherActor && OtherActor != GetInstigator())
 	{
--- Source/ActionRoguelike/Public/SMagicProjectile.h
+++ Source/ActionRoguelike/Public/SMagicProjectile.h
@@ -27,6 +27,7 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Audio")
 	UAudioComponent *AudioComp;
-	
-	virtual void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) override;	
+
+	UFUNCTION()
+	void OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
