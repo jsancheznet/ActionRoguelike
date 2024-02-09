20f95d77431e4e02225d1856dd8ee3fd8e9b8563
jsanchez.net <jsanchezsilvera@gmail.com>
2024-02-09 12:26:21 -0300

    Only the server can now add/remove player credits

--- Source/ActionRoguelike/Private/Scharacter.cpp
+++ Source/ActionRoguelike/Private/Scharacter.cpp
@@ -7,6 +7,8 @@
 #include "SInteractionComponent.h"
 #include "Camera/CameraComponent.h"
 #include "SAttributeComponent.h"
+#include "SPlayerState.h"
+#include "ActionRoguelike/ActionRoguelike.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Kismet/KismetMathLibrary.h"
@@ -58,25 +60,27 @@ void AScharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// -- Rotation Visualization -- //
-	const float DrawScale = 100.0f;
-	const float Thickness = 5.0f;
+	{ // -- Character Rotation Visualization -- //
+		// const float DrawScale = 100.0f;
+		// const float Thickness = 5.0f;
+		// FVector LineStart = GetActorLocation();
+		// Offset to the right of pawn
+		// LineStart += GetActorRightVector() * 100.0f;
+		// Set line end in direction of the actor's forward
+		// FVector ActorDirection_LineEnd = LineStart + (GetActorForwardVector() * 100.0f);
+		// Draw Actor's Direction
+		// DrawDebugDirectionalArrow(GetWorld(), LineStart, ActorDirection_LineEnd, DrawScale, FColor::Yellow, false, 0.0f, 0, Thickness);
+		// FVector ControllerDirection_LineEnd = LineStart + (GetControlRotation().Vector() * 100.0f);
+		// Draw 'Controller' Rotation ('PlayerController' that 'possessed' this character)
+		// DrawDebugDirectionalArrow(GetWorld(), LineStart, ControllerDirection_LineEnd, DrawScale, FColor::Green, false, 0.0f, 0, Thickness);		
+	}
 
-	FVector LineStart = GetActorLocation();
-	// Offset to the right of pawn
-	LineStart += GetActorRightVector() * 100.0f;
-	// Set line end in direction of the actor's forward
-	FVector ActorDirection_LineEnd = LineStart + (GetActorForwardVector() * 100.0f);
-	// Draw Actor's Direction
-	// DrawDebugDirectionalArrow(GetWorld(), LineStart, ActorDirection_LineEnd, DrawScale, FColor::Yellow, false, 0.0f, 0, Thickness);
-
-	FVector ControllerDirection_LineEnd = LineStart + (GetControlRotation().Vector() * 100.0f);
-	// Draw 'Controller' Rotation ('PlayerController' that 'possessed' this character)
-	// DrawDebugDirectionalArrow(GetWorld(), LineStart, ControllerDirection_LineEnd, DrawScale, FColor::Green, false, 0.0f, 0, Thickness);
-
-	// FString MyRage = FString::Printf(TEXT("Rage: %d"), AttributeComp->GetRage());
-	// GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Red, *MyRage);
-
+	ASPlayerState *PS = Cast<ASPlayerState>(GetPlayerState());
+	if(PS)
+	{
+		FString Msg = FString::Printf(TEXT("Credits: %d : %s"), PS->GetCredits(), *GetNameSafe(this));
+		LogOnScreen(GetWorld(), Msg, FColor::White, 0.0f);
+	}
 }
 
 // Called to bind functionality to input
